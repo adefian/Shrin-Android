a2956368408339acd0c33093609c6ff2d7096b2b
Ade Fian Galih I <fianade8@gmail.com>
2020-10-03 23:42:11 +0700

    MDC-103 login fragment

--- app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardRecyclerViewAdapter.java
+++ app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardRecyclerViewAdapter.java
@@ -3,14 +3,13 @@ package com.google.codelabs.mdc.java.shrine;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.codelabs.mdc.java.shrine.network.ImageRequester;
 import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
 
 import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Adapter used to show a simple grid of products.
@@ -34,7 +33,12 @@ public class ProductCardRecyclerViewAdapter extends RecyclerView.Adapter<Product
 
     @Override
     public void onBindViewHolder(@NonNull ProductCardViewHolder holder, int position) {
-        // TODO: Put ViewHolder binding code here in MDC-102
+        if (productList != null && position < productList.size()) {
+            ProductEntry product = productList.get(position);
+            holder.productTitle.setText(product.title);
+            holder.productPrice.setText(product.price);
+            imageRequester.setImageFromUrl(holder.productImage, product.url);
+        }
     }
 
     @Override
